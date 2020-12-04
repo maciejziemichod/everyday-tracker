9c7e7dcca3a2337f8202a193f44e47f252c38281
maciejziemichod <maciej.ziemichod@gmail.com>
2020-12-04 17:04:37 +0100

    build: :sparkles: added gh pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  lintOnSave: false
-}
+  lintOnSave: false,
+
+  // gh pages deployment
+  publicPath: "/everyday-tracker/",
+
+  // for anything other that gh pages'
+  //   publicPath: "/",
+
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Everyday Tracker";
+      return args;
+    });
+  },
+};
